670b0feaee8b62d46488bba4a026981ba7359bcf
kike-pinto <cripintort@gmail.com>
2022-05-01 22:06:42 -0400

    initial commit

---
+++ src/components/SearchForm.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useGlobalContext } from '../context'
+
+const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext()
+  const searchValue = React.useRef('')
+
+  React.useEffect(() => {
+    searchValue.current.focus()
+  }, [])
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <section className='section search'>
+      <div className='search-form' onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <label htmlFor='name'>search your favorite cocktail</label>
+          <input
+            type='text'
+            id='name'
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default SearchForm
---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+        rerum iste facere fugiat hic asperiores officia? Quibusdam vel
+        repellendus non esse, hic obcaecati dicta rem, quia minima nisi quaerat
+        distinctio fuga id necessitatibus excepturi officiis quo nobis ducimus
+        quasi molestias.
+      </p>
+    </section>
+  )
+}
+
+export default About
